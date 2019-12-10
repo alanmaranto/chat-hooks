9dc86ce94e33157c5a94304178c9ed19467194ef
alanmaranto <alanarostegui@hotmail.com>
2019-12-10 15:14:54 -0600

    creando eldashboard

---
+++ src/views/Dashboard/Dashboard.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        margin: '50px',
+        padding: theme.spacing(3, 2),
+    },
+    flex: {
+        display: "flex"
+    },
+    topicsWindow: {
+        width: '30%',
+        height: '300px',
+        borderRight: '1px solid grey'
+    },
+    chatWindow: {
+        width: '70%',
+        height: '300px'
+    },
+    chatBox: {
+        width: '85%'
+    },
+    button: {
+        width: '15%'
+    } 
+  }));
+
+const Dashboard = () => {
+
+    const classes = useStyles();
+
+    return ( 
+        <div>
+            <Paper className={classes.root}>
+                <Typography variant="h4" component="h4">
+                    Chat App
+                </Typography>
+                <Typography variant="h5" component="h5">
+                    Topic placeholder
+                </Typography>
+                <div className={classes.flex}>
+                    <div className={classes.topicsWindow}>
+
+                    </div>
+                    <div className={classes.chatWindow}>
+                        
+                    </div>
+                </div>
+                <div className={classes.flex}>                        
+                </div>
+            </Paper>
+        </div>
+     );
+}
+ 
+export default Dashboard;
