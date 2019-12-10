2877497b570c203092bd5782ede53cf9cc3444c6
alanmaranto <alanarostegui@hotmail.com>
2019-12-10 16:11:40 -0600

    agreggando list, chip, button y textfield

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,61 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Chip from "@material-ui/core/Chip";
+import Button from "@material-ui/core/Button";
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        margin: '50px',
-        padding: theme.spacing(3, 2),
-    },
-    flex: {
-        display: "flex"
-    },
-    topicsWindow: {
-        width: '30%',
-        height: '300px',
-        borderRight: '1px solid grey'
-    },
-    chatWindow: {
-        width: '70%',
-        height: '300px'
-    },
-    chatBox: {
-        width: '85%'
-    },
-    button: {
-        width: '15%'
-    } 
-  }));
+  root: {
+    margin: "50px",
+    padding: theme.spacing(3, 2)
+  },
+  flex: {
+    display: "flex",
+    alignItems: "center"
+  },
+  topicsWindow: {
+    width: "30%",
+    height: "300px",
+    borderRight: "1px solid grey"
+  },
+  chatWindow: {
+    width: "70%",
+    height: "300px",
+    padding: "20px"
+  },
+  chatBox: {
+    width: "85%"
+  },
+  button: {
+    width: "15%"
+  }
+}));
 
 const Dashboard = () => {
+  const classes = useStyles();
 
-    const classes = useStyles();
+  const [textValue, changeTextValue] = useState('');
 
-    return ( 
-        <div>
-            <Paper className={classes.root}>
-                <Typography variant="h4" component="h4">
-                    Chat App
-                </Typography>
-                <Typography variant="h5" component="h5">
-                    Topic placeholder
-                </Typography>
-                <div className={classes.flex}>
-                    <div className={classes.topicsWindow}>
-
-                    </div>
-                    <div className={classes.chatWindow}>
-                        
-                    </div>
-                </div>
-                <div className={classes.flex}>                        
-                </div>
-            </Paper>
+  return (
+    <div>
+      <Paper className={classes.root}>
+        <Typography variant="h4" component="h4">
+          Chat App
+        </Typography>
+        <Typography variant="h5" component="h5">
+          Topic placeholder
+        </Typography>
+        <div className={classes.flex}>
+          <div className={classes.topicsWindow}>
+            <List>
+              {["topic"].map(topic => (
+                <ListItem key={topic} button>
+                  <ListItemText primary={topic} />
+                </ListItem>
+              ))}
+            </List>
+          </div>
+          <div className={classes.chatWindow}>
+            {[{ from: "user", msg: "hello" }].map((chat, i) => (
+              <div className={classes.flex} key={i}>
+                <Chip label={chat.from} className={classes.chip} />
+                <Typography variant="p">{chat.msg}</Typography>
+              </div>
+            ))}
+          </div>
         </div>
-     );
-}
- 
+        <div className={classes.flex}>
+        <TextField 
+            label="Send a chat"
+            className={classes.chatBox}
+            value={textValue}
+            onChange={(e) => changeTextValue(e.target.value)} 
+        />
+          <Button variant="contained" color="primary">
+            SEND
+          </Button>
+        </div>
+      </Paper>
+    </div>
+  );
+};
+
 export default Dashboard;
